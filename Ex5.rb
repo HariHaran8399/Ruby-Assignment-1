75053f61c5cb6b245ebbe78482aa60a95190ccc8
HariHaran8399 <122720373+HariHaran8399@users.noreply.github.com>
2023-01-19 10:29:12 +0530

    Add files via upload

---
+++ Ex5.rb
@@ -0,0 +1,18 @@
+my_name = "hari"
+my_age = 24
+my_height = 179.8 #cm
+my_weight = 90  #kg
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "brown"
+
+height =  my_height / 2.54
+weight = my_weight * 2.205
+
+puts "Let's talk about #{my_name}."
+puts "He's #{height.round(2)} inches tall."
+puts "He's #{weight.round(2)} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "If I add #{my_age} ,#{my_height} ,and #{my_weight} I get #{my_age+ my_height + my_weight}"
